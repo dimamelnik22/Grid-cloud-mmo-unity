a0d31a9432fde78fc3a5c33c6c514f9cc0bbafe2
CTAPblU_MEJlbHUK <dim.meln@yandex.ru>
2020-12-03 22:52:39 +0300

    Rough prototype

---
+++ New Unity Project/Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20f;
+	private PlayerMotor motor;
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = Vector3.right * _xMov;
+        Vector3 _moveVertical = Vector3.forward * _zMov;
+
+	Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+
+	motor.Move(_velocity);
+    }
+}
---
+++ New Unity Project/Assets/PlayerMotor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+
+public class PlayerMotor : MonoBehaviour
+{
+    public Vector3 velocity = Vector3.zero;
+	
+	private Rigidbody rb;
+    void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	void  Update()
+    {
+		
+	}
+	public void Move(Vector3 _velocity)
+	{
+		velocity = _velocity;
+	}
+    	void FixedUpdate()
+	{
+		PerformMovemnt();
+		transform.rotation = Quaternion.identity;
+	}
+
+	void PerformMovemnt()
+	{
+		if (velocity != Vector3.zero)
+		{
+			
+			rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+		}
+		rb.velocity = Vector3.zero;
+		rb.angularVelocity = Vector3.zero;
+
+	}
+}

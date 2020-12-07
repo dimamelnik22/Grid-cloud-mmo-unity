bd39daf96ed8ff05b4b6851743fe1f66fd89442a
vlad23 <vlalbusel@gmail.com>
2020-12-07 14:03:48 +0300

    Shooting and rotation

--- New Unity Project/Assets/PlayerController.cs
+++ New Unity Project/Assets/PlayerController.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20f;
+    private float speed = 5f;
 	private PlayerMotor motor;
+    public Rigidbody rb;
+    public Camera cam;
+    public GameObject bulletSpawnPoint;
+    public GameObject bullet;
+    private Transform bulletSpawned;
+
+    Vector3 mousePos;
+
     void Start()
     {
         motor = GetComponent<PlayerMotor>();
@@ -15,12 +23,27 @@ public class PlayerController : MonoBehaviour
     {
         float _xMov = Input.GetAxisRaw("Horizontal");
         float _zMov = Input.GetAxisRaw("Vertical");
+        mousePos = Input.mousePosition;
 
-        Vector3 _moveHorizontal = Vector3.right * _xMov;
-        Vector3 _moveVertical = Vector3.forward * _zMov;
 
-	Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+	    Vector3 _velocity = (Vector3.right * _xMov + Vector3.forward * _zMov).normalized * speed;
+        rb.MovePosition(rb.position + _velocity * Time.fixedDeltaTime);
 
-	motor.Move(_velocity);
+        Vector3 targetRotation =  mousePos - cam.WorldToScreenPoint(rb.position);
+        float angle = Mathf.Atan2(targetRotation.x, targetRotation.y) * Mathf.Rad2Deg - 90f;
+        rb.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+
+	    // motor.Move(_velocity);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+           Shoot(); 
+        }
+    }
+
+    void Shoot()
+    {
+        bulletSpawned = Instantiate(bullet.transform, bulletSpawnPoint.transform.position, Quaternion.identity);
+        bulletSpawned.rotation = bulletSpawnPoint.transform.rotation;
     }
 }
--- New Unity Project/Assets/PlayerMotor.cs
+++ New Unity Project/Assets/PlayerMotor.cs
@@ -17,18 +17,21 @@ public class PlayerMotor : MonoBehaviour
 	public void Move(Vector3 _velocity)
 	{
 		velocity = _velocity;
+		// mousePos = _mousePos;
 	}
     	void FixedUpdate()
 	{
 		PerformMovemnt();
-		transform.rotation = Quaternion.identity;
+		// transform.rotation = Quaternion.identity;
+		// Vector2 lookDir = mousePos - (Vector2.right*rb.position.z + Vector2.up*rb.position.x);
+		// float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+		// rb.rotation = Quaternion.AngleAxis(angle, Vector3.up);
 	}
 
 	void PerformMovemnt()
 	{
 		if (velocity != Vector3.zero)
-		{
-			
+		{	
 			rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
 		}
 		rb.velocity = Vector3.zero;
